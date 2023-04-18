c91242e93e000c9c40b71d38c7c6aaf35b9c6133
Owen <ojdowds@gmail.com>
2023-04-18 09:17:30 +0100

    new updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SkillsListComponent } from './skills-list/skills-list.component';
+import { SkillEditComponent } from './skills-edit/skills-edit.component';
 
 const routes: Routes = [
-  { path: 'skills', component: SkillsListComponent }
+  { path: 'skills', component: SkillsListComponent },
+  {
+    path: 'skills-edit/:id',
+    component: SkillEditComponent
+  },
 ];
 
 @NgModule({
